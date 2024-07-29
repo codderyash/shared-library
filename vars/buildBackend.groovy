f92eb6c1e5ca4857ac03442128ba3abe8eeab74b
yash <yashsarvaiya30575@gmail.com>
2024-07-29 21:36:05 +0530

    asd

--- vars/buildBackend.groovy
+++ vars/buildBackend.groovy
@@ -10,8 +10,8 @@ def call(body) {
              checkout scm
 
               checkout([$class: 'GitSCM', branches: [[name: '*/main']],
-                                  userRemoteConfigs: [[url: scriptRepoUrl]],
-                                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'script-repo']]])
+                                  userRemoteConfigs: [[url: 'https://github.com/codderyash/terraform-pipeline-test-2.git']],
+                                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: './terraform-pipeline-2']]])
          }
         
       
@@ -20,10 +20,10 @@ def call(body) {
         script{
             input "Do you want to deploy all resources to aws?"
 
-            dir('C:/Users/YASH/.jenkins/workspace/terraform-test_main/terraform-pipeline-test-2'){
+            dir('./terraform-pipeline-test-2'){
   withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
                     credentialsId: '16552b1e-b971-4018-93f4-abee8968c654',
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                 ]]){
                   
